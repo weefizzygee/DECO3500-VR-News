d925e5880b65f59cadc1befa0d474690e0663355
weefizzygee <williamwilesmith@hotmail.com.au>
2014-09-11 15:09:59 +1000

    Creating some interaction

--- DiveTrial/Assets/Scripts/Select.cs
+++ DiveTrial/Assets/Scripts/Select.cs
@@ -14,7 +14,20 @@ public class Select : MonoBehaviour {
 		Debug.DrawRay (transform.position, transform.forward * 50);
 		if(Physics.Raycast(ray, out hit, 50)) 
 		{
-			Debug.Log("HIT");
+			Debug.Log("HIT!");
+			if (hit.collider.tag == "box") {
+				Debug.Log("It's a box!");
+				StartCoroutine(changeColourGreen(hit.collider.gameObject.renderer));
+			}
 		}
+	}
+
+	IEnumerator changeColourGreen (Renderer rend) {
+		Color colour = rend.material.color;
+		rend.material.color = Color.green;
+		Debug.Log("Turned it green!");
+		yield return new WaitForSeconds(1);
+		rend.material.color = Color.gray;
+		Debug.Log("Returned to normal!");
 	}
 }
