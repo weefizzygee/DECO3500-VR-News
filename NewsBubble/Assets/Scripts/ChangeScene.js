3b67b24a79e9fcd10216e567ad7c3f92d184ce25
yanbo-zhao <zhaoyanbo_ivy@hotmail.com>
2014-09-30 14:36:45 +1000

    Splash_screen, use Sarah's bubble design

---
+++ NewsBubble/Assets/Scripts/ChangeScene.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+function Start () {
+	// Start_Screen showing for 20 seconds before entering map view
+	yield WaitForSeconds (10);
+
+	Application.LoadLevel(1);
+}
+
+function Update () {
+
+}
---
+++ NewsBubble/Assets/Scripts/Select.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Select : MonoBehaviour {
+	public RaycastHit hit;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Ray ray = new Ray (transform.position, transform.forward);
+		Debug.DrawRay (transform.position, transform.forward * 50);
+		if(Physics.Raycast(ray, out hit, 50)) 
+		{
+			Debug.Log("HIT");
+		}
+	}
+}
