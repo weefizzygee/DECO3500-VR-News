9af0868ce73a90c67ee529b692f1f3b2a60dde11
yanbo-zhao <zhaoyanbo_ivy@hotmail.com>
2014-10-03 02:07:25 +1000

    vote function trail

---
+++ NewsBubble/Assets/Scripts/Vote.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+var likes : int;
+
+function Start () {
+	if(PlayerPrefs.GetInt("vote") == null){
+			
+			PlayerPrefs.SetInt("vote", 0);
+	}		
+	likes = PlayerPrefs.GetInt("vote");
+}
+
+function OnMouseDown () {
+	likes++;
+	PlayerPrefs.SetInt("vote", likes);
+}
+
+function OnGUI () {
+	GUI.Label(Rect(240,250,160,40), "Like: " + PlayerPrefs.GetInt("vote"));
+}
