0ac07f15a6bc2e3789a56bb6fbaccf9ac0ae50b3
weefizzygee <williamwilesmith@hotmail.com.au>
2014-10-19 16:08:27 +1000

    Something else

---
+++ NewsBubble/Assets/Scripts/escapeApplication.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class escapeApplication : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		 if (Input.GetKeyDown (KeyCode.Escape)) {
+			if (Application.loadedLevelName.Equals("ArticleView")){
+				Application.Quit();
+			} else {
+				Application.LoadLevel("ArticleView");
+			}
+		} else {
+		}
+	}
+}
